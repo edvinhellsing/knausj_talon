5ead9e2f6611a50c5338091d554f451229299889
JWaldenback <joel.waldenback@outlook.com>
2022-09-22 09:35:17 +0200

    Add command "insert" to insert words or phrases from the list additional_words.csv

---
+++ code/additional_words.py
@@ -0,0 +1,36 @@
+"""
+This module gives us the list {user.additional_words} to easily refer to system paths in talon and python files
+"""
+from talon import Context, Module
+
+from .user_settings import get_list_from_csv
+
+mod = Module()
+mod.list("additional_words", desc="List of additional words")
+
+additional_words_defaults = {
+    #"Spoken form": "Words(s)",
+}
+
+ctx = Context()
+ctx.lists["self.additional_words"] = get_list_from_csv(
+    "additional_words.csv",
+    headers=("Word(s)", "Spoken Form (If Different)"), default=additional_words_defaults,
+)
+
+"""
+# We need to wait for ready before we can call "actions.path.talon_home()" and
+# "actions.path.talon_user()"
+def on_ready():
+    additional_words = get_list_from_csv(
+        "additional_words.csv", headers=("Word(s)", "Spoken Form (If Different)"), default=additional_words_defaults
+    )
+
+    ctx.lists["user.additional_words"] = additional_words
+
+@mod.capture(rule="{user.additional_words}")
+def additional_words(m) -> str:
+    return m.additional_words
+
+app.register("ready", on_ready)
+"""
--- misc/my_customizations/my_customizations.talon
+++ misc/my_customizations/my_customizations.talon
@@ -1,6 +1,6 @@
 #A general advice if I may, try to find a quite idiomatic way for short commands because you risk to have difficulty to remember them when you would have 50 of them. Personally i use a y suffix for most of my short commands: lessy, listy, movy, taily, awky and so forth
 
-#Question. in knausj_talon what is the difference between edit.find() and user.find()?
+#Question. In knausj_talon what is the difference between edit.find() and user.find()?
 #If you find them in actions.list() they should both have descriptions
 
 #Otherwise, you should check out events.tail() in the repl to see what's happening behind the scenes for specific commands
@@ -13,6 +13,7 @@
 #test select start: user.select_continous(1)
 #test select end: user.select_continous(0)
 
+insert {user.additional_words}: auto_insert(additional_words) 
 website {user.website}: auto_insert(website)
 system path <user.system_path>: auto_insert(system_path)
 
