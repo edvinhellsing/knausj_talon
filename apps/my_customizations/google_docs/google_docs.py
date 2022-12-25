256177038db8f73c2b6a0692996fb0009a259979
JWaldenback <joel.waldenback@outlook.com>
2022-12-25 15:20:54 +0100

    Enabled Google's own voice typing for Google Docs instead of using Talons dictation mode

--- apps/my_customizations/google_docs/google_docs.py
+++ apps/my_customizations/google_docs/google_docs.py
@@ -1,14 +1,5 @@
 from talon import Module, actions, Context, app, scope
 
-
-# mod = Module()
-
-# @mod.action_class
-# class Actions:
-#     def docs_start_stop_dictation():
-#         """Start native dictation on both Windows and macOS"""
-
-
 ctx_native_dictation=Context()
 ctx_native_dictation.matches = r"""
 os: windows
@@ -19,8 +10,7 @@ browser.host: docs.google.com
 
 ctx_native_dictation.tags = ["user.docs"]
 
-"""
-#Comment out this part as it currently doesn't seem to work using Google's own dictation for some reason
+#Commented out this part as it currently doesn't seem to work using Google's own dictation for some reason
 @ctx_native_dictation.action_class("user")
 class NativeDictationActions:
     def start_stop_dictation():
@@ -32,8 +22,8 @@ class NativeDictationActions:
         #To make it work in Firefox small alterations need to be done in Firefox itself
         #Open about:config from your address bar
         #Set extensions.screenshots.disabled to true
-"""
 
+"""
 @ctx_native_dictation.action_class("user")
 class NativeDictationActions:
     def toggle_dictation_voice_command():
@@ -53,16 +43,6 @@ class NativeDictationActions:
             actions.user.engine_mimic("command mode")
             actions.user.mouse_wake()
 
-        # current_microphone = actions.sound.active_microphone()
-        # if current_microphone == "None":
-        #     actions.user.engine_mimic("command mode")
-        #     actions.user.mouse_wake()
-        # else:
-        #     actions.user.mouse_sleep()
-        #     actions.user.engine_mimic("dictation mode")
-        #     actions.user.toggle_talon_microphone()
-        #     actions.user.start_stop_dictation()
-
     def toggle_dictation_key_switch():
         current_microphone = actions.sound.active_microphone()
         if "command" in scope.get("mode"):
@@ -79,3 +59,4 @@ class NativeDictationActions:
             #Currently power mode is not supported, need to utilize a stack to know which mode to go back to
             actions.user.engine_mimic("command mode")
             actions.user.mouse_wake()
+"""
